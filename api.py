724c7f6010ba39acba453d2417620663f3523124
vitorhiratsuca <vitorhiratsuca@gmail.com>
2026-03-17 09:06:49 -0300

    Implement initial API for listing properties by city
    Co-authored-by: Mateus Ahn <mateusjwa@al.insper.edu.br>

--- api.py
+++ api.py
@@ -182,3 +182,29 @@ def listar_imoveis_por_tipo(tipo):
     }
 
     return jsonify(response), 200
+
+
+@app.route('/imoveis/cidade/<string:cidade>', methods=['GET'])
+def listar_imoveis_por_cidade(cidade):
+    conn = conectar_banco()
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT id, logradouro, tipo_logradouro, bairro, cidade, cep, tipo, valor, data_aquisicao FROM imoveis WHERE cidade = ?', (cidade,))
+    rows = cursor.fetchall()
+    
+    cursor.close()
+    conn.close()
+
+    data = [{
+        'id': row[0],
+        'logradouro': row[1],
+        'tipo_logradouro': row[2],
+        'bairro': row[3],
+        'cidade': row[4],
+        'cep': row[5],
+        'tipo': row[6],
+        'valor': row[7],
+        'data_aquisicao': row[8]
+    } for row in rows]
+
+    return jsonify(data), 200
